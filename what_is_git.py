0893646fbac55e2525d3289860bdf352be888c3b
Bob <Lady.Jokeresp@protonmail.com>
2022-09-27 20:09:14 +0900

    aboutメソッドを多言語対応にする

--- what_is_git.py
+++ what_is_git.py
@@ -1,6 +1,6 @@
 class WhatIsGit:
-    def about(self):
-        print('https://en.wikipedia.org/wiki/Git')
+    def about(self, lang = 'en'):
+        print(f'https://{lang}.wikipedia.org/wiki/Git')
 
     def show(self):
         print('Git is easy if you understand the basis.')

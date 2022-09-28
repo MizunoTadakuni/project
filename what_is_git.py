beca9b3e54e58bbff0ffa3aea3fb410e07d894d7
Bob <Lady.Jokeresp@protonmail.com>
2022-09-28 20:10:25 +0900

    showメソッドに引数を設定

--- what_is_git.py
+++ what_is_git.py
@@ -2,5 +2,5 @@ class WhatIsGit:
     def about(self, lang = 'en'):
         print(f'https://{lang}.wikipedia.org/wiki/Git')
 
-    def show(self):
-        print('if you understand the basis, Git is easy.')
+    def show(self, msg='easy'):
+        print(f'Git is {msg}')

f2bc0e51d577912d6e95498f4691605f83e9abf8
Bob <Lady.Jokeresp@protonmail.com>
2022-09-29 13:42:14 +0900

    showメソッドを変更(クラス定義の先頭を小文字に修正)

--- what_is_git.py
+++ what_is_git.py
@@ -2,5 +2,5 @@ class WhatIsGit:
     def about(self, lang = 'en'):
         print(f'https://{lang}.wikipedia.org/wiki/Git')
 
-    def show(self, msg='easy'):
+    def show(self, msg='easy!'):
         print(f'Git is {msg}')
